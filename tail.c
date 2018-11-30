0cfd795b71015baaff81d37d1024649ba780291b
Anthony127 <909655@gmail.com>
2018-11-30 16:33:45 -0500

    Changed file variable name to make more sense

--- tail.c
+++ tail.c
@@ -5,37 +5,34 @@
  
 void main() 
 { 
-	FILE* in;
-	in = fopen("/var/log/syslog", "r");
-	if(in == NULL)
+	FILE* logFile;
+	logFile = fopen("/var/log/syslog", "r");
+	if(logFile == NULL)
 	{
 		perror("error");
 	}
 	int n = 500;
     int count = 0;  // To count '\n' characters 
   
-    // unsigned long long pos (stores upto 2^64 – 1 
-    // chars) assuming that long long int takes 8  
-    // bytes 
     unsigned long long pos; 
     char str[200]; 
   
     // Go to End of file 
-    if (fseek(in, 0, SEEK_END)) 
+    if (fseek(logFile, 0, SEEK_END)) 
         perror("fseek() failed"); 
     else
     { 
         // pos will contain no. of chars in 
         // input file. 
-        pos = ftell(in); 
+        pos = ftell(logFile); 
   
         // search for '\n' characters 
         while (pos) 
         { 
             // Move 'pos' away from end of file. 
-            if (!fseek(in, --pos, SEEK_SET)) 
+            if (!fseek(logFile, --pos, SEEK_SET)) 
             { 
-                if (fgetc(in) == '\n') 
+                if (fgetc(logFile) == '\n') 
   
                     // stop reading when n newlines 
                     // is found 
@@ -48,7 +45,7 @@ void main()
   
         // print last n lines 
         //printf("Printing last %d lines -\n", n); 
-        while (fgets(str, sizeof(str), in)){
+        while (fgets(str, sizeof(str), logFile)){
    				const char s[2] = "]";
 				const char s2[2] = " ";
    				char *token;
